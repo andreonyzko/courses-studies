c15d4c1468a71c5082491d68ddfb37e321657ae2
André Onyszko <145982643+andreonyzko@users.noreply.github.com>
2024-05-02 17:04:54 -0300

    aula11

--- aula11/ex001.py
+++ aula11/ex001.py
@@ -1,1 +1,5 @@
-print('\33[0;33;44mOlá, Mundo!\33[m')
+print('\033[31mOlá, Mundo!')
+print('\033[31;43mOlá, Mundo!\033[m')
+print('\033[4;30;45mOlá, Mundo!\033[m')
+print('\033[7;33;44mOlá, Mundo!\033[m')
+print('Os valores são \033[32m{}\033[m e \033[31m{}\033[m!!'.format(1,3))
