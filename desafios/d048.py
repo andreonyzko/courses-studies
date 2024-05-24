5bcd076f577a6691b913bfeb80b3458335cb5e2b
André Onyszko <145982643+andreonyzko@users.noreply.github.com>
2024-05-23 22:29:29 -0300

    d046 -> d056

--- desafios/d048.py
+++ desafios/d048.py
@@ -1,5 +1,5 @@
 soma = 0
-for c in range(1,500,2):
+for c in range(1,501,2):
     if c%3==0:
         print(c)
         soma+= c
--- desafios/d049.py
+++ desafios/d049.py
@@ -1,3 +1,3 @@
 n = int(input('Número para tabuar: '))
 for c in range(0,11,1):
-    print('{} x {} = {}'.format(n,c,n*c))
+    print('{} x {:2} = {}'.format(n,c,n*c))
